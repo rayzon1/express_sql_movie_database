2e94b5320ddb9888093438d96a1dfbe4a3f04874
Gerardo Keys <jotojry@icloud.com>
2019-08-02 17:55:31 -0700

    commit

---
+++ routes/new 2.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var Article = require('../models').Article;
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    Article.findAll()
+        .then(article => {
+            res.render('new',{ title: 'Create a new entry', articles: article });
+        })
+  
+});
+
+router.post('/', (req,res,next) => {
+    Article.create(req.body)
+        .then( () => {
+            res.redirect('/create')
+        })
+})
+
+module.exports = router;
